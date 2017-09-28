5506dba09982bcac9184e224f4171c7ce660ba3f
WillemijnJens <32366091+WillemijnJens@users.noreply.github.com>
2017-09-28 18:04:17 +0200

    eerste commit

---
+++ Routing/Routing/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Routing.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome
+        public string Hello(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+
+        public string Welcome(string name, string group, string year)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", Group: " + group + " From Year: " + year);
+        }
+    }
+}
